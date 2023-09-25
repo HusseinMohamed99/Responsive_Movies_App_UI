6b84834994d032dc0160cf13acbc7afcb22dcd08
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-09-26 02:34:35 +0300

    Added imports and functions related to UI design and removed unnecessary imports and functions.✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movies_app_ui/screens/OnBoarding/onboarding_screen.dart';
+import 'package:movies_app_ui/utils/Sizes/size_config.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,15 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    SizeConfig().init(context);
     return MaterialApp(
-
-        title: 'Material App', 
-               debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: GoogleFonts.openSans().fontFamily,
         ),
         home: const OnBoardingScreen());
-  
   }
 }
--- lib/screens/OnBoarding/onboarding_screen.dart
+++ lib/screens/OnBoarding/onboarding_screen.dart
@@ -1,10 +1,69 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:movies_app_ui/utils/Colors/colors_manager.dart';
+import 'package:movies_app_ui/utils/Sizes/size_config.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: ColorsManager.kBlackColor,
+      body: SizedBox(
+        width: SizeConfig.screenWidth,
+        height: SizeConfig.screenHeight,
+        child: Stack(
+          children: [
+            Positioned(
+              top: SizeConfig.screenHeight! * 0.1,
+              left: -88,
+              child: Container(
+                width: 166,
+                height: 166,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: ColorsManager.kPinkColor,
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 200,
+                    sigmaY: 200,
+                  ),
+                  child: const SizedBox(
+                    width: 166,
+                    height: 166,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: SizeConfig.screenHeight! * 0.3,
+              right: -100,
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: ColorsManager.kGreenColor,
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 200,
+                    sigmaY: 200,
+                  ),
+                  child: const SizedBox(
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
